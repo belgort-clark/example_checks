c5b2629a64141f5c9d7ff1bbcbeeb0d7f5d1b0f4
Bruce Elgort - Instructor <belgort@clark.edu>
2022-04-28 18:09:52 -0700

    Update __init__.py

--- example/__init__.py
+++ example/__init__.py
@@ -3,7 +3,7 @@ import check50 # import the check50 module
 @check50.check() # tag the function below as check50 check
 def exists(): # the name of the check
   """File hello.py exists""" # this is what you will see when running check50
-  check50.exists("hellos.py") # the actual check
+  check50.exists("hello.py") # the actual check
   
 @check50.check(exists) # only run this check if the exists check has passed
 def prints_hello():
@@ -13,4 +13,5 @@ def prints_hello():
 @check50.check(exists)
 def gets_input():
   """Inputs the names Fred and Jim and then prints them"""
-  check50.run("python3 hello.py").stdin("Fred").stdout("Fred").stdin("Jim").stdout("Jims").exit(0)
+  check50.run("python3 hello.py").stdin("Fred").stdout("Fred").stdin("Jim").stdout("Jims").stdout("Program Ended").exit(0)
+  
